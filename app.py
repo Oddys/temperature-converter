88bc941130bb68f68cc75a4897c5e3758af93388
Oddys <yuriy.honcharenko@gmail.com>
2017-07-25 12:45:12 +0300

    Modify add.py to make a real app

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class Converter:
 
         tkinter.Button(self.frame, text='Convert', command=self.convert).pack()
         tkinter.Button(self.frame, text='Quit',
-                       command=self.master.destroy).pack()
+                       command=self.quit).pack()
 
     def converter(self):
         """Convert temperature from one scale (stored in self.temp1) to
@@ -36,6 +36,12 @@ class Converter:
             self.converter()
         except ValueError:
             self.temp2.set('ENTER YOUR TEMPERATURE')
+
+    def quit(self):
+        """Clear and hide the parent frame."""
+        for widget in self.master.winfo_children():
+            widget.destroy()
+        self.master.pack_forget()
 
 
 class FahrenheitToCelsius(Converter):
@@ -56,3 +62,36 @@ class CelsiusToFahrenheit(Converter):
 
     def converter(self):
         self.temp2.set(float(self.temp1.get()) * 9 / 5 + 32)
+
+
+class AppFrame:
+    """The main application frame."""
+    def __init__(self, master):
+        self.master = master
+        self.frame1 = tkinter.Frame(self.master, borderwidth=10)
+        self.frame1.pack()
+        self.frame2 = tkinter.Frame(self.master)
+
+        self.converters = {
+            'Fahrenheit To Celsius': FahrenheitToCelsius,
+            'Celsius To Fahrenheit': CelsiusToFahrenheit,
+        }
+        for name in self.converters:
+            tkinter.Button(
+                self.frame1, text=name,
+                command=lambda c=self.converters[name]: self.run(c)).pack()
+
+    def run(self, converter):
+        # If user has switched converters without closing the frame, clear it
+        for widget in self.frame2.winfo_children():
+            widget.destroy()
+
+        if not self.frame2.winfo_ismapped():  # the frame isn't packed
+            self.frame2.pack()
+        converter(self.frame2)
+
+
+if __name__ == '__main__':
+    window = tkinter.Tk()
+    app = AppFrame(window)
+    window.mainloop()
